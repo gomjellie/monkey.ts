cc12d5a82ca496e26023da42f0dc434ca25af51f
gomjellie <gomjellie@gmail.com>
2021-11-08 08:51:52 +0900

    feat: repl works

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import {init} from './repl';
+
+console.log('Wellcome to Monkey Language!');
+init(process.stdin, process.stdout);
---
+++ src/repl/index.ts
@@ -0,0 +1,22 @@
+import {Lexer} from '../lexer';
+
+const PROMPT = '> ';
+
+const init = (stdin: NodeJS.ReadStream, stdout: NodeJS.WriteStream) => {
+  stdin.setEncoding('utf8');
+  stdin.on('data', (data: string) => {
+    stdout.write(PROMPT);
+    const line = data.trim();
+    const l = new Lexer(line);
+
+    for (
+      let token = l.nextToken();
+      token.type !== 'EOF';
+      token = l.nextToken()
+    ) {
+      console.log(token);
+    }
+  });
+};
+
+export {init};
