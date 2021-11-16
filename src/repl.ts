ab60d35f7a1f7e66e9e0476a4ed9f6aec0080937
gomjellie <gomjellie@gmail.com>
2021-11-16 17:16:50 +0900

    feat: make repl to print parser results

--- src/repl.ts
+++ src/repl.ts
@@ -1,22 +1,51 @@
+import {STATUS_CODES} from 'http';
 import {Lexer} from './lexer';
+import {Parser} from './parser';
 
 const PROMPT = '> ';
+const MONKEY_FACE = String.raw`
+            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`;
 
 const init = (stdin: NodeJS.ReadStream, stdout: NodeJS.WriteStream) => {
+  stdout.write(PROMPT);
   stdin.setEncoding('utf8');
   stdin.on('data', (data: string) => {
-    stdout.write(PROMPT);
     const line = data.trim();
     const l = new Lexer(line);
+    const p = new Parser(l);
+    const program = p.parseProgram();
 
-    for (
-      let token = l.nextToken();
-      token.type !== 'EOF';
-      token = l.nextToken()
-    ) {
-      console.log(token);
+    if (!program) {
+      stdout.write('Parse error\n');
+      return;
     }
+
+    if (p.errors.length > 0) {
+      printParseErrors(stdout, p.errors);
+    }
+
+    stdout.write(program.toString());
+    stdout.write('\n');
+    stdout.write(PROMPT);
   });
 };
+
+function printParseErrors(stdout: NodeJS.WriteStream, errors: string[]) {
+  stdout.write(MONKEY_FACE);
+  stdout.write('Woops! We ran into some monkey business here!\n');
+  stdout.write('parser errors:\n');
+  stdout.write(`${errors.join('\n')}`);
+}
 
 export {init};
